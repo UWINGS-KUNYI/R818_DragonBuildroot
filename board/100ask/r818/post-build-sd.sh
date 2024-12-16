8c0a1b7999768d9e9554758be81bd9449b4376d3
Yuxuan <livelwh@outlook.com>
2024-12-16 06:22:54 -0500

    fix build image error

--- board/100ask/r818/post-build-sd.sh
+++ board/100ask/r818/post-build-sd.sh
@@ -5,17 +5,19 @@ BOARD_DIR="$(dirname $0)"
 echo "Target binary dir $BOARD_DIR"
 
 
-#cp $BOARD_DIR/env-sd.cfg -rfvd  $BINARIES_DIR
-#cp $BOARD_DIR/boot_package.cfg -rfvd  $BINARIES_DIR
-#cp $BOARD_DIR/bootlogo.bmp.lzma -rfvd  $BINARIES_DIR
-#cp $BOARD_DIR/bootlogo.bmp -rfvd  $BINARIES_DIR
-#cp $BOARD_DIR/ramdisk.img -rfvd  $BINARIES_DIR
+cp $BOARD_DIR/env-sd.cfg -rfvd  $BINARIES_DIR
+cp $BOARD_DIR/boot_package.cfg -rfvd  $BINARIES_DIR
+cp $BOARD_DIR/bootlogo.bmp.lzma -rfvd  $BINARIES_DIR
+cp $BOARD_DIR/bootlogo.bmp -rfvd  $BINARIES_DIR
+cp $BOARD_DIR/ramdisk.img -rfvd  $BINARIES_DIR
+
+#cp $BOARD_DIR/../dragon/* -rfvd  $BINARIES_DIR
 
 # Copy some system bins.
-#cp $BOARD_DIR/../pack_img/* -rfvd  $BINARIES_DIR
+cp $BOARD_DIR/../pack_img/* -rfvd  $BINARIES_DIR
 
 #Copy tina pack tools
-#cp $BOARD_DIR/../tina-pack-tools/* -rfvd  $BINARIES_DIR
+cp $BOARD_DIR/../tina-pack-tools/* -rfvd  $BINARIES_DIR
 
 
 #cd buildroot/output/images/
